a834b88c071b2affe97f63050872b15676831217
kirin111697 <coco41134@hotmail.com>
2016-03-31 11:32:35 +0800

    change file name

--- main.cpp
+++ main.cpp
@@ -1,9 +1,13 @@
-#include "sudoku.h"
+#include "Sudoku.h"
 using namespace std;
 
 int main(){
 	Sudoku sudoku;
 	sudoku.readIn();
+	sudoku.transform();
+	cout << endl << endl;
 	sudoku.solve();
+	cout << endl << endl;
+
 	return 0;
 }
